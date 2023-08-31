02bd6819c1707df3effe1cb4ab8be9c4754dffad
PaciStardust <pacistardust@gmail.com>
2023-08-31 16:34:13 +0200

    Credibility text filters

---
+++ Discord/UserCredibility/CredibilityRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarisBot.Discord.UserCredibility
+{
+    internal abstract class CredibilityRule
+    {
+        internal string Name { get; init; }
+        internal int Score { get; init; }
+
+        internal abstract bool Evaluate(string input); //todo: return a result?
+
+        internal CredibilityRule(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+    }
+}
