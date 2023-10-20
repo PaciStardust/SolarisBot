661f7cb235f404c633e66f12d8966f4941baee1d
Paci <89483951+PaciStardust@users.noreply.github.com>
2023-10-20 13:19:11 +0200

    Include missing includes

--- Discord/Commands/QuoteCommands.cs
+++ Discord/Commands/QuoteCommands.cs
@@ -96,7 +96,7 @@ namespace SolarisBot.Discord.Commands
             };
 
             _logger.LogDebug("{intTag} Adding quote {quote} by user {user} to guild {guild}", GetIntTag(), dbQuote, Context.User.Log(), Context.Guild.Log());
-            _dbContext.Quotes.Add(dbQuote);
+            guild.Quotes.Add(dbQuote);
             await _dbContext.SaveChangesAsync();
             _logger.LogInformation("{intTag} Added quote {quote} by user {user} to guild {guild}", GetIntTag(), dbQuote, Context.User.Log(), Context.Guild.Log());
             await Interaction.ReplyAsync(GetQuoteEmbed(dbQuote));
