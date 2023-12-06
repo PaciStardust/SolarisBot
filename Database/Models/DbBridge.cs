fe82bd26e8045f2e63558a6b676b63ad2483e0f2
Paci <89483951+PaciStardust@users.noreply.github.com>
2023-12-06 04:57:32 +0100

    Bridge datamodel and sql

---
+++ Database/Models/DbBridge.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SolarisBot.Database.Models
+{
+    [PrimaryKey(nameof(BridgeId))]
+    internal class DbBridge
+    {
+        public ulong BridgeId { get; set; } = ulong.MinValue;
+        public ulong ChannelAId { get; set; } = ulong.MinValue;
+        public ulong ChannelBId { get; set; } = ulong.MinValue;
+
+        public DbBridge() { }
+
+        public override string ToString()
+            => $"{BridgeId}: {ChannelAId} <-> {ChannelBId}";
+    }
+
+    internal static class DbBridgeExtensions
+    {
+        internal static IQueryable<DbBridge> ForChannel(this IQueryable<DbBridge> query, ulong id)
+            => query.Where(x => x.ChannelBId == id || x.ChannelAId == id);
+    }
+}
