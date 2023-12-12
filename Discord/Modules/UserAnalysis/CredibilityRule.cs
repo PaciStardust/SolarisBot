72b373263cf1a1a866a2214c655f16b88fc9e07c
Paci <89483951+PaciStardust@users.noreply.github.com>
2023-12-12 13:50:10 +0100

    CredibilityRule classes

---
+++ Discord/Modules/UserAnalysis/CredibilityRule.cs
@@ -0,0 +1,14 @@
+﻿namespace SolarisBot.Discord.Modules.UserAnalysis
+{
+    internal abstract class CredibilityRule
+    {
+        public string Name { get; init; }
+        public int Score { get; init; }
+
+        internal CredibilityRule(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+    }
+}
---
+++ Discord/Modules/UserAnalysis/TimeCredibilityRule.cs
@@ -0,0 +1,15 @@
+﻿namespace SolarisBot.Discord.Modules.UserAnalysis
+{
+    internal sealed class TimeCredibilityRule : CredibilityRule
+    {
+        public TimeSpan MinimumAge { get; init; }
+
+        internal TimeCredibilityRule(string name, int score, TimeSpan minimumAge) : base(name, score)
+        {
+            MinimumAge = minimumAge;
+        }
+
+        internal bool IsCredible(DateTime dateTime)
+            => DateTime.Now - dateTime >= MinimumAge;
+    }
+}
